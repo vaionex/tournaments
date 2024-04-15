d138c5bf5105e771868c0345e4308268374bac86
Saad A <saad@vaionex.com>
2024-04-15 18:34:51 +0500

    feat: seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,33 @@ import "./globals.css";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
+const title = "Tournaments.com";
+const description =
+  "Join Tournaments.com for the ultimate gaming and earn rewards";
 export const metadata = {
-  title: "Tournaments.com",
-  description: "Tournaments.com",
+  title,
+  description,
+  openGraph: {
+    url: "https://tournaments.com",
+    title,
+    description,
+    images: [
+      {
+        url: `https://tournaments.com/card.webp`, // Must be an absolute URL
+        width: 1200,
+        height: 615,
+        alt: "Tournaments.com",
+      },
+    ],
+    siteName: "Tournaments.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@Tournaments.com",
+    images: [`https://tournaments.com/card.webp`],
+  },
 };
 
 export default function RootLayout({ children }) {
