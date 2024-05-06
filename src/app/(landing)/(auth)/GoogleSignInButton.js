62a09ba44861a64780d79ea778d66ed30fead8ac
Saad A <saad@vaionex.com>
2024-05-06 18:40:56 +0500

    fix: absolute redirect url

--- src/app/(landing)/(auth)/GoogleSignInButton.js
+++ src/app/(landing)/(auth)/GoogleSignInButton.js
@@ -2,12 +2,24 @@ import { Button } from "@/components/ui/button";
 import { supabase } from "@/supabase/client";
 import toast from "react-hot-toast";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return `${url}dashboard`;
+};
+
 export default function GoogleSignInButton() {
   async function handleClick() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "/dashboard",
+        redirectTo: getURL(),
       },
     });
     if (error) toast.error(error.message ?? "An unexpected error occured");
