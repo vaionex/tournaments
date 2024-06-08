6889610bf515a373b3baec14a75a3943cdaf7349
Saad A <saad@vaionex.com>
2024-06-08 21:12:04 +0500

    fix: change redirect for logged in users

--- src/app/(landing)/components/cta/index.js
+++ src/app/(landing)/components/cta/index.js
@@ -1,26 +1,35 @@
+"use client";
+
 import Container from "@/components/ui/container";
-import Logo from "@/components/ui/logo";
 import LogoMark from "@/components/ui/logo-mark";
 import { Button } from "@/components/ui/button";
+import useAuthentication from "@/hooks/auth/useAuthentication";
 
 export default function CTA() {
+  const { isAuthenticated } = useAuthentication();
+
   return (
     <Container
-      className="text-center space-y-8 !py-16 mb-24"
+      className="mb-24 space-y-8 !py-16 text-center"
       style={{
         backgroundImage: `url(/images/cta-background.webp)`,
         backgroundSize: "cover",
       }}
     >
-      <LogoMark className="h-10 mx-auto" />
+      <LogoMark className="mx-auto h-10" />
       <h2 className="text-4xl font-bold">Start Your Journey to Glory</h2>
-      <div className="max-w-2xl mx-auto text-xl">
+      <div className="mx-auto max-w-2xl text-xl">
         Whether you&apos;re here to make your mark as a legendary organizer or
         to rise through the ranks as a champion competitor, your path begins
         with a single click.
       </div>
       <div className="flex items-center justify-center gap-4">
-        <Button variant="white" href="/signup">Join a Game</Button>
+        <Button
+          variant="white"
+          href={isAuthenticated ? "/dashboard" : "/signup"}
+        >
+          Join a Game
+        </Button>
         {/* <Button>Create a Tournament</Button> */}
       </div>
     </Container>
