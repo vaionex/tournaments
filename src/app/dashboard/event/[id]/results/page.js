6bc7665b03b2e50fac960048d7fe95403285bc2b
Saad A <saad@vaionex.com>
2024-06-08 23:40:31 +0500

    fix: edge cases for results page

--- src/app/dashboard/event/[id]/results/page.js
+++ src/app/dashboard/event/[id]/results/page.js
@@ -1,13 +1,34 @@
-import { getParticipants } from "@/db/tournament";
+import { getParticipants, getTournament } from "@/db/tournament";
 import Podium from "./components/Podium";
 
 export default async function Results({ params: { id } }) {
   const participants = await getParticipants(id);
+  const tournament = await getTournament(id);
+
+  if (!tournament.completed) {
+    return (
+      <div className="py-24 text-center text-2xl font-semibold text-neutral-400">
+        Tournament is still in progress
+      </div>
+    );
+  }
+
+  if (participants.length == 0) {
+    return (
+      <div className="py-24 text-center text-2xl font-semibold text-neutral-400">
+        No Participants
+      </div>
+    );
+  }
+
   return (
     <div className="flex items-center justify-center gap-8 px-12 py-8">
-      {participants.slice(0, 3).map(({ User }, index) => (
-        <Podium position={index + 1} {...User} />
-      ))}
+      {participants
+        .sort(({ score: a }, { score: b }) => b - a)
+        .slice(0, 3)
+        .map(({ User }, index) => (
+          <Podium position={index + 1} {...User} />
+        ))}
     </div>
   );
 }
