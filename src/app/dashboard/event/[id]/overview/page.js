dcf83172d1a115d8bf5caec8610d383f72a9ab48
Saad A <saad@vaionex.com>
2024-06-13 21:15:22 +0500

    feat: update tournament overview page

--- src/app/dashboard/event/[id]/overview/page.js
+++ src/app/dashboard/event/[id]/overview/page.js
@@ -1,7 +1,9 @@
 import { getParticipants, getTournament } from "@/db/tournament";
 import { formatCurrency } from "@/utils/format";
 import { format } from "date-fns";
-import { Swords } from "lucide-react";
+import { Calendar, Swords, Users } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+import { Trophy01, Users01 } from "untitledui-js-base";
 
 export const revalidate = 0;
 
@@ -11,53 +13,55 @@ export default async function Overview({ params: { id } }) {
 
   const stats = [
     {
-      name: "Next Match",
-      value: "Phoenix Rises Vs Solar Sentinels",
-      icon: Swords,
+      name: "Prize Pool",
+      value: formatCurrency(prize_pool),
+      icon: Trophy01,
+      iconClassName: "text-lime-500",
+      image: "prize-pool",
     },
     {
-      name: "Remaining Teams",
-      value: participants.length,
-      icon: Swords,
-    },
-    {
-      name: "Eliminated Match",
-      value: "0",
-      icon: Swords,
+      name: "Date",
+      value: `${format(start, "Pp")} - ${format(end, "Pp")}`,
+      icon: Calendar,
+      iconClassName: "text-indigo-600",
+      image: "date",
     },
     {
       name: "Participants",
       value: participants.length,
-      icon: Swords,
+      icon: Users01,
+      iconClassName: "text-indigo-600",
+      image: "participants",
     },
     {
-      name: "Date",
-      value: `${format(start, "dd/MM/yyyy")} - ${format(end, "dd/MM/yyyy")}`,
-      icon: Swords,
-    },
-    {
-      name: "Prize Pool",
-      value: formatCurrency(prize_pool),
-      icon: Swords,
+      name: "Remaining Teams",
+      value: participants.length,
+      icon: Users01,
+      iconClassName: "text-yellow-600",
+      image: "remaining",
     },
   ];
 
   return (
-    <div className="">
-      <div className="grid grid-cols-3 gap-px bg-neutral-700 pb-px">
-        {stats.map(({ name, value, icon: Icon }) => (
-          <div className="flex flex-col justify-between gap-8 bg-black px-6 py-5">
-            <div className="flex items-center gap-4 text-xs text-neutral-400">
-              <Icon className="size-4" />
+    <div className="p-4">
+      <div className="grid grid-cols-2 gap-4 pb-px">
+        {stats.map(({ name, value, icon: Icon, iconClassName, image }) => (
+          <div
+            className="flex flex-col justify-between gap-6 rounded-xl border border-white/10 bg-black bg-contain bg-right bg-no-repeat px-6 py-5"
+            style={{
+              backgroundImage: `url('/images/dashboard/tournament/overview/${image}.webp')`,
+            }}
+          >
+            <div className="flex items-center gap-2 text-xs text-neutral-400">
+              <Icon className={twMerge("size-5", iconClassName)} />
               {name}
             </div>
-            <div className="text-sm">{value}</div>
+            <div className="text-sm font-medium text-neutral-400">{value}</div>
           </div>
         ))}
       </div>
-      <div className="p-4">
-        <img src={banner} alt={name} className="rounded-md object-cover" />
-      </div>
+
+      <img src={banner} alt={name} className="mt-4 rounded-md object-cover" />
     </div>
   );
 }
