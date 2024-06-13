6fdb566c447844405393372ee7619ad9841d9ec8
Saad A <saad@vaionex.com>
2024-06-13 16:44:01 +0500

    fix: input font size

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -15,7 +15,7 @@ const Input = React.forwardRef(
             {label}
           </label>
         )}
-        <div className="flex w-full items-center rounded-md border border-neutral-700 bg-black text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus:bg-red-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300">
+        <div className="flex w-full items-center rounded-md border border-neutral-700 bg-black ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus:bg-red-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300">
           {leftSection && <div className="ml-3">{leftSection}</div>}
           <Comp
             type={type}
