f6063d548d21db61f84ac0bcf0579809108ca0e8
Saad A <saad@vaionex.com>
2024-06-14 13:05:53 +0500

    fix: share alignment

--- src/app/(landing)/news/[slug]/SignupSidebar.js
+++ src/app/(landing)/news/[slug]/SignupSidebar.js
@@ -5,7 +5,7 @@ import useAuthentication from "@/hooks/auth/useAuthentication";
 export default function SignupSidebar() {
   const { isUnauthenticated } = useAuthentication();
 
-  if (!isUnauthenticated) return null;
+  if (!isUnauthenticated) return <div className="w-96" />;
 
   return (
     <div className="h-fit w-96 rounded-xl border border-neutral-700 bg-gradient-to-b from-white/10 p-8">
