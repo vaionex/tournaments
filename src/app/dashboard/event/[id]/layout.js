c467f4586333c1733e17685bb3c1791c3dc9d863
Saad A <saad@vaionex.com>
2024-06-17 23:05:32 +0500

    feat: hide matchmaking page if no participants

--- src/app/dashboard/event/[id]/layout.js
+++ src/app/dashboard/event/[id]/layout.js
@@ -1,4 +1,4 @@
-import { getTournament } from "@/db/tournament";
+import { getParticipants, getTournament } from "@/db/tournament";
 import MenuLink from "./MenuLink";
 import { notFound } from "next/navigation";
 import { getUser } from "@/supabase/server";
@@ -41,6 +41,7 @@ export async function generateMetadata({ params: { id } }) {
 
 export default async function TournamentLayout({ children, params: { id } }) {
   const tournament = await getTournament(id);
+  const participants = await getParticipants(id);
   if (!tournament) return notFound();
 
   const { name, user_id, start, entry_fee } = tournament;
@@ -49,6 +50,7 @@ export default async function TournamentLayout({ children, params: { id } }) {
   const isOwner = user?.id == user_id;
   const isStarted = new Date(start) < new Date();
   const canEdit = isOwner && !isStarted;
+  const hasParticipants = participants.length != 0;
 
   return (
     <div>
@@ -65,9 +67,14 @@ export default async function TournamentLayout({ children, params: { id } }) {
       </div>
       <div className="flex border-t border-white/20">
         <div className="flex flex-col  p-1 pr-8 pt-6 text-sm font-medium">
-          {links.map(({ icon, ...link }) => (
-            <MenuLink {...link} key={link.href} />
-          ))}
+          {links
+            .filter(({ name }) => {
+              if (name == "Matchmaking Key" && !hasParticipants) return false;
+              return true;
+            })
+            .map(({ icon, ...link }) => (
+              <MenuLink {...link} key={link.href} />
+            ))}
         </div>
         <div className="h-full flex-1 ">
           <div className="border-b border-l border-r border-white/20">
