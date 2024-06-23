73fe22067e19081dcb392ba73697007eec3b61db
Saad A <saad@vaionex.com>
2024-06-23 16:00:51 +0500

    fix: wrong import

--- src/app/dashboard/event/[id]/JoinTournamentButton.js
+++ src/app/dashboard/event/[id]/JoinTournamentButton.js
@@ -4,6 +4,8 @@ import useUser from "@/hooks/auth/useUser";
 import useIsParticipant from "@/hooks/tournament/useIsParticipant";
 import useJoinTournament from "@/hooks/tournament/useJoinTournament";
 import useParticipants from "@/hooks/tournament/useParticipants";
+import { socials } from "../../profile/account/page";
+import Link from "next/link";
 
 export default function JoinTournamentButton({
   entryFee,
@@ -17,13 +19,25 @@ export default function JoinTournamentButton({
     useIsParticipant(tournamentId);
   const { data: participants = [] } = useParticipants(tournamentId);
   const { balance } = user;
+
   const insufficientBalance = entryFee > balance;
   const isStarted = new Date().valueOf() > new Date(start).valueOf();
+  const hasConnectedSocial = socials.some((social) => user[social]);
 
   if (isParticipant) return <div>Joined</div>;
   if (isStarted || isLoadingParticipations) return null;
 
   if (participants.length >= maxPlayers) return <div>Tournament Full</div>;
+
+  if (!hasConnectedSocial)
+    return (
+      <div>
+        Social not connected.{" "}
+        <Link href="/dashboard/profile/account#social" className="text-primary">
+          Connect Social
+        </Link>
+      </div>
+    );
 
   return (
     <div>
