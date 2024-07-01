b0a4db12a5b8e406103db7871498b183b3eb9d94
Saad A <saad@vaionex.com>
2024-07-01 14:31:29 +0500

    fix: alignment

--- src/components/tournament/tournament-form/index.js
+++ src/components/tournament/tournament-form/index.js
@@ -367,7 +367,7 @@ export default function TournamentForm({
       </div> */}
 
       <div>Rules</div>
-      <div className="space-y-4 p-4">
+      <div className="space-y-4 py-4">
         {rules.map(({ title, description }, index) => (
           <div className="rounded-lg border border-white/20 p-4" key={index}>
             <div className="mb-2">
