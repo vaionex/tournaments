8b373e4483e5793b1c653806c6a53b45f91b4a26
Saad A <saad@vaionex.com>
2024-09-07 16:31:20 +0500

    feat: create tournament in dropdown

--- src/app/dashboard/profile-dropdown/index.js
+++ src/app/dashboard/profile-dropdown/index.js
@@ -17,6 +17,10 @@ export default function ProfileDropdown() {
 
   const items = [
     isAdmin && { name: "Admin", href: "/admin" },
+    isAdmin && {
+      name: "Create tournament",
+      href: "/dashboard/tournaments/create",
+    },
     { name: "Dashboard", href: "/dashboard/" },
     { name: "Profile", href: "/dashboard/profile/account" },
     { name: "Log out", onClick: () => logout() },
@@ -39,14 +43,14 @@ export default function ProfileDropdown() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right space-y-0.5 rounded bg-neutral-900 p-2 shadow-lg ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-fit origin-top-right space-y-0.5 rounded bg-neutral-900 p-2 shadow-lg ring-opacity-5 focus:outline-none">
           {items.map(({ name, href, onClick }) => (
             <Menu.Item key={name}>
               {({ active }) => (
                 <button
                   className={twMerge(
                     active ? "bg-white/10" : "",
-                    "block w-full px-2.5 py-2 text-left text-sm",
+                    "block w-full whitespace-nowrap px-2.5 py-2 text-left text-sm",
                   )}
                   onClick={() => (href ? push(href) : onClick?.())}
                 >
