51b8935f4033d633d75ad0781d636d9ae9b36650
kohze <robin@kohze.com>
2024-12-23 15:19:03 +0530

    fixing banner issue

--- src/app/dashboard/event/[id]/overview/page.js
+++ src/app/dashboard/event/[id]/overview/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 // Replace img tags with Image component
 <Image 
-  src={banner}
-  alt={name}
+  src={banner || "/images/default-banner.jpg"} 
+  alt={name || "Tournament banner"}
   width={1200}
   height={630}
   className="rounded-md object-cover"
