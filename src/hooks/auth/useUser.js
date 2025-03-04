464bd800edfbafefdd3d91cf9d1312ae46ee10be
kohze <robin@kohze.com>
2025-03-04 20:01:22 +0530

    fixed logged in user error

--- src/hooks/auth/useUser.js
+++ src/hooks/auth/useUser.js
@@ -16,22 +16,26 @@ export default function useUser() {
     queryKey: ["user"],
     initialData: null,
     queryFn: async () => {
-      const { data, error: authError } = await supabase.auth.getUser();
-      if (!data || authError) throw authError;
+      const { data: authData, error: authError } = await supabase.auth.getUser();
+      if (!authData?.user || authError) throw authError;
+
       const { data: users, error } = await supabase
         .from("User")
         .select("*")
-        .eq("id", data.user.id)
+        .eq("id", authData.user.id)
         .throwOnError();
+
       if (error) throw error;
-      return { ...data.user, ...users[0] };
+      if (!users?.length) throw new Error("User data not found");
+
+      return { ...authData.user, ...users[0] };
     },
   });
 
   const newQuery = useMemo(() => {
     if (!query.data) return query;
     
-    const xp = query.data?.xp ?? 0;
+    const xp = query.data.xp ?? 0;
     return {
       ...query,
       data: {
